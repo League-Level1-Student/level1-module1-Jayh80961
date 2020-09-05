8d951dbb112648ee8f90fd7d221acbabd6077f53
Jayh80961 <59904920+Jayh80961@users.noreply.github.com>
2020-09-05 11:26:43 -0700

    did good

---
+++ src/_02_cat/CatRunner.java
@@ -0,0 +1,25 @@
+package _02_cat;
+
+public class CatRunner {
+	public static void main(String[] args) {
+		Cat bob = new Cat("Jeff");
+		bob.meow();
+		bob.printName();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+		bob.kill();
+	}
+}
